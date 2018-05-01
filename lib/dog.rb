9d2ddc3dc48bb487578ef006ada205d9ff9a5c9d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-01 23:54:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,31 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.create (name:, breed:)
+    new_dog = Dog.new(name: name, breed: breed)
+    new_dog.save
+    new_dog
+  end
+
+  def self.find_by_id (id)
+    sql = "SELECT * FROM dogs WHERE id=?"
+    row = DB[:conn].execute(sql).first
+    Dog.new(id: row[0], name: row[1], breed: row[2])
+  end
+  
+  def save
+    if !id
+      sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    else
+      update
+    end
+  end
+
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
 end
